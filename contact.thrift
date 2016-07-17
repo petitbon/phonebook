696cde4983845d9b00f783840a1a960ad0fb9ad0
petitbon <petitbon@gmail.com>
2016-07-16 20:30:33 -0400

    first commit

---
+++ contact.thrift
@@ -0,0 +1,16 @@
+struct Contact {
+    1: required string id,
+    2: required string name,
+    3: required string phone,
+    4: optional string email,
+    5: required string created,  // RFC3339
+}
+
+service ContactSvc {
+    Contact create(1:Contact contact),
+    Contact read(1:string contactId),
+    Contact update(1:Contact contact),
+    void destroy(1:string contactId),
+//    list fetch(),
+//    void reset(),
+}
---
+++ server.go
@@ -0,0 +1,51 @@
+package phonebook
+
+import (
+	"fmt"
+
+	"github.com/petitbon/phonebook/services/go/contact"
+
+	"git-wip-us.apache.org/repos/asf/thrift.git/lib/go/thrift"
+)
+
+type PhonebookServer struct {
+	host             string
+	handler          *ContactHandler
+	processor        *contact.ContactSvcProcessor
+	transport        *thrift.TServerSocket
+	transportFactory thrift.TTransportFactory
+	protocolFactory  *thrift.TBinaryProtocolFactory
+	server           *thrift.TSimpleServer
+}
+
+func NewPhonebookServer(host string) *PhonebookServer {
+	handler := NewContactHandler()
+	processor := contact.NewContactSvcProcessor(handler)
+	transport, err := thrift.NewTServerSocket(host)
+	if err != nil {
+		panic(err)
+	}
+
+	transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
+	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+	server := thrift.NewTSimpleServer4(processor, transport, transportFactory, protocolFactory)
+	return &PhonebookServer{
+		host:             host,
+		handler:          handler,
+		processor:        processor,
+		transport:        transport,
+		transportFactory: transportFactory,
+		protocolFactory:  protocolFactory,
+		server:           server,
+	}
+}
+
+func (ps *PhonebookServer) Run() {
+	fmt.Printf("server listening on %s\n", ps.host)
+	ps.server.Serve()
+}
+
+func (ps *PhonebookServer) Stop() {
+	fmt.Println("stopping server...")
+	ps.server.Stop()
+}
